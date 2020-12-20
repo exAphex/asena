57cf15322a9c2630203e1233c9005cf137bbc933
Aydin Tekin <aydin.tekin@ibsolution.de>
2020-12-20 16:54:23 +0100

    added connectionpropertyservice tests

--- src/main/java/com/asena/scimgateway/service/ConnectionPropertyService.java
+++ src/main/java/com/asena/scimgateway/service/ConnectionPropertyService.java
@@ -3,6 +3,8 @@ package com.asena.scimgateway.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import com.asena.scimgateway.exception.NotFoundException;
 import com.asena.scimgateway.model.ConnectionProperty;
 import com.asena.scimgateway.model.RemoteSystem;
@@ -12,6 +14,7 @@ import com.asena.scimgateway.repository.RemoteSystemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Transactional
 @Service
 public class ConnectionPropertyService {
     @Autowired
@@ -36,6 +39,11 @@ public class ConnectionPropertyService {
         .orElseThrow(() -> new NotFoundException(id));
     }
 
+    public ConnectionProperty create(ConnectionProperty cp) {
+        return connectionPropertyRepository.save(cp);
+    }
+
+    
     public void delete(ConnectionProperty cp) {
         List<RemoteSystem> rs = remoteSystemRepository.findByPropertiesId(cp.getId());
         for (RemoteSystem r : rs) {
@@ -53,5 +61,13 @@ public class ConnectionPropertyService {
             return a;
         })
         .orElseThrow(() -> new NotFoundException(id));
+    }
+
+    
+    public void deleteAll() {
+        List<ConnectionProperty> cps = connectionPropertyRepository.findAll();
+        for (ConnectionProperty cp : cps) {
+            delete(cp);
+        }
     }
 }
---
+++ src/test/java/com/asena/scimgateway/service/ConnectionPropertyServiceTest.java
@@ -0,0 +1,120 @@
+package com.asena.scimgateway.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.asena.scimgateway.exception.NotFoundException;
+import com.asena.scimgateway.model.ConnectionProperty;
+import com.asena.scimgateway.model.RemoteSystem;
+import com.asena.scimgateway.model.ConnectionProperty.ConnectionPropertyType;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConnectionPropertyServiceTest {
+    
+    @Autowired
+    private ConnectionPropertyService connectionPropertyService;
+
+    @Autowired
+    private RemoteSystemService remoteSystemService;
+
+    @BeforeEach
+    void prepareDb() {
+        connectionPropertyService.deleteAll();
+    }
+
+    @Test
+    void createTest() {
+        ConnectionProperty cp = new ConnectionProperty();
+        cp.setDescription("Testdesc");
+        cp.setEncrypted(false);
+        cp.setKey("TEST");
+        cp.setType(ConnectionPropertyType.STRING);
+        cp.setValue("Testval");
+
+        cp = connectionPropertyService.create(cp);
+
+        assertEquals("Testdesc", cp.getDescription());
+        assertEquals(false, cp.isEncrypted());
+        assertEquals("TEST", cp.getKey());
+        assertEquals(ConnectionPropertyType.STRING, cp.getType());
+        assertEquals("Testval", cp.getValue());
+    }
+
+    @Test
+    void findByIdTest() {
+        ConnectionProperty cp = new ConnectionProperty();
+        cp.setDescription("Testdesc");
+        cp.setEncrypted(false);
+        cp.setKey("TEST");
+        cp.setType(ConnectionPropertyType.STRING);
+        cp.setValue("Testval");
+
+        cp = connectionPropertyService.create(cp);
+
+        connectionPropertyService.findById(cp.getId()).orElseThrow(() -> new NotFoundException(0l));
+
+        assertThrows(NotFoundException.class, () -> {
+            connectionPropertyService.findById(0).orElseThrow(() -> new NotFoundException(0l));
+        });
+    }
+
+    @Test
+    void updateTest() {
+        ConnectionProperty cp = new ConnectionProperty();
+        cp.setDescription("Testdesc");
+        cp.setEncrypted(false);
+        cp.setKey("TEST");
+        cp.setType(ConnectionPropertyType.STRING);
+        cp.setValue("Testval");
+
+        cp = connectionPropertyService.create(cp);
+
+        cp.setDescription("Testdesc1");
+        cp.setEncrypted(true);
+        cp.setKey("TEST1");
+        cp.setType(ConnectionPropertyType.INT);
+        cp.setValue("Testval1");
+        
+        cp = connectionPropertyService.update(cp, cp.getId());
+
+        assertEquals("Testdesc1", cp.getDescription());
+        assertEquals(true, cp.isEncrypted());
+        assertEquals("TEST1", cp.getKey());
+        assertEquals(ConnectionPropertyType.STRING, cp.getType());
+        assertEquals("Testval1", cp.getValue());
+
+        assertThrows(NotFoundException.class, () -> {
+            connectionPropertyService.update(null, 0);
+        });
+    }
+
+    @Test
+    void deleteTest() {
+        ConnectionProperty cp = new ConnectionProperty();
+        cp.setDescription("Testdesc");
+        cp.setEncrypted(false);
+        cp.setKey("TEST");
+        cp.setType(ConnectionPropertyType.STRING);
+        cp.setValue("Testval");
+
+        cp = connectionPropertyService.create(cp);
+
+        RemoteSystem rs = new RemoteSystem();
+        rs.setType("LDAP");
+        rs.setName("TESTSYS");
+        rs.addProperty(cp);
+        rs = remoteSystemService.create(rs);
+
+        connectionPropertyService.deleteById(cp.getId());
+
+
+        assertThrows(NotFoundException.class, () -> {
+            connectionPropertyService.deleteById(0);
+        });
+    }
+}
