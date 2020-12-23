63f65383c2b357a504bd44d2332db1f62b79f1ff
exAphex <aydin.tekin.66@hotmail.de>
2020-12-23 03:24:29 +0100

    cleanup

--- database/init.sql
+++ database/init.sql
@@ -11,8 +11,5 @@ CREATE TABLE IF NOT EXISTS public.users (
 	PRIMARY KEY(user_id)
 );
 
-INSERT INTO public.users (user_id, department, first_name, last_name, user_name) VALUES(nextval('public.users_sequence'), 'SAP Security F&E', 'Aydin', 'Tekin', 'atekin');
-INSERT INTO public.users (user_id, department, first_name, last_name, user_name) VALUES(nextval('public.users_sequence'), 'SAP Security Consulting', 'Marcus', 'Schleppe', 'mschleppe');
-INSERT INTO public.users (user_id, department, first_name, last_name, user_name) VALUES(nextval('public.users_sequence'), 'SAP Security Customer Success', 'Tobias', 'Mehlhorn', 'tmehlhorn');
 
 COMMIT;
