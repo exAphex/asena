271cc1a816955e8a243319871166ab4a797b4e56
exAphex <aydin.tekin.66@hotmail.de>
2020-12-26 15:18:34 +0100

    added userdetailsservice tests

---
+++ src/test/java/com/asena/scimgateway/security/service/AUserDetailsServiceTest.java
@@ -0,0 +1,39 @@
+package com.asena.scimgateway.security.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.asena.scimgateway.exception.NotFoundException;
+import com.asena.scimgateway.model.User;
+import com.asena.scimgateway.service.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@SpringBootTest
+public class AUserDetailsServiceTest {
+    @Autowired
+    private AUserDetailsService aUserDetailsService;
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void loadUserByUsernameTest() {
+        User u = userService.createServiceUser("TEST");
+
+        UserDetails ud = aUserDetailsService.loadUserByUsername(u.getUserName());
+        assertEquals(u.getUserName(), ud.getUsername());
+        
+        assertThrows(UsernameNotFoundException.class, () -> {
+            aUserDetailsService.loadUserByUsername(null);
+        });
+
+        assertThrows(UsernameNotFoundException.class, () -> {
+            aUserDetailsService.loadUserByUsername("");
+        });
+    }
+}
